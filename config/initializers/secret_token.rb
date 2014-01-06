d1caae75973a0f9b347f7ae4b400286be4986d03
mrdelurk <mrdelurk@mrdelurk-VirtualBox.(none)>
2014-01-05 17:38:09 -1000

    first test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embedder::Application.config.secret_key_base = '30e7943cafb92ce831bd265808dbc88d8f1000660bb85f8cef55a25e3f801e9ac457d47e5a6408aa430ca1133c7cb9ecfa3e13137fcf9e405de6a96687348c76'
